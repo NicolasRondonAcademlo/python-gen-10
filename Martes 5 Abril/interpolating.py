2d60569b622955462fd2454336fdd6e5dcf87b47
NicolasRondonAcademlo <96024979+NicolasRondonAcademlo@users.noreply.github.com>
2022-04-05 20:13:20 -0500

    data

---
+++ Martes 5 Abril/interpolating.py
@@ -0,0 +1,29 @@
+n = 20
+m = 25
+prod = n*m 
+print("The product of ", n, "and", m , "is", prod)
+result = "The product of ", n, "and", m , "is", prod
+
+result = f"The product of {n} and {m} is {prod}"
+print(result)
+
+
+first = "Eric"
+last_name = "Idle"
+age = 74
+profession = "comedian"
+print("Hello {0} {1} is a {2} of {3} years old".format(
+    first, last_name, profession, age
+))
+
+name = input("Ingresa tu nombre: ")
+
+age = int(input("Ingresa tu edad "))
+print(type(age))
+message = f"Hola {name} {age}"
+print(message)
+
+
+# 
+
+
--- Martes 5 Abril/strings.py
+++ Martes 5 Abril/strings.py
@@ -30,11 +30,23 @@ print(string not in string_large)
 # len()
 # str()
 
-print(ord("a"))
-print(chr(97))
+print(ord("@")) # ASCCII
+print(chr(64))
 
 large_string = "This is a large string"
 print(len(large_string))
 
 number = 456
 print(str(number) + "15")
+
+foo = "foobar"
+print(foo[2:-1])
+
+
+s = "foobar"
+#-> "2 paso a la drecha"
+print(s[0::2])
+
+print(s[5:0:2])
+#<- 2 pasos a la izquierda
+
